115755e93af39c30ad6db356fed033e6f1e7236a
nwerka <tideg37@gmail.com>
2022-08-10 16:47:18 -0700

    added js

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+// Variables
+var lastname = "Washington";
+let firstname = "George";
+const hobby = "kayaking";
+
+const isHappy = false;
+
+// Data types <- think of them as the categories that can be placed inside a variable
+
+/* DATA TYPES
+// String 
+// Numbers
+// Boolean (George Boole) - true or false
+// null
+// undefined
+// Arrays
+// Objects
+// Big Int
+// Symbol
+
+console.log(firstname);
+console.log(lastname);
